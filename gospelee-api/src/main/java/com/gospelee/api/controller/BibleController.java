46a28dfcdab935edb9843dbd4155dc716a065a76
jej8076 <jej8076@gmail.com>
2023-11-15 16:47:28 +0900

    컨트롤러 이름 변경

--- gospelee-api/src/main/java/com/gospelee/api/controller/BibleController.java
+++ gospelee-api/src/main/java/com/gospelee/api/controller/BibleController.java
@@ -20,13 +20,13 @@ public class BibleController {
     private final String REGEX_KOR = ".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*";
 
     @GetMapping("/{book}/{chapter}")
-    public ResponseEntity<Object> getBibleBy(@PathVariable(name = "book") Integer book, @PathVariable(name = "chapter") Integer chapter) {
+    public ResponseEntity<Object> getBibleByBookAndChapter(@PathVariable(name = "book") Integer book, @PathVariable(name = "chapter") Integer chapter) {
         return new ResponseEntity<>(bibleService.findByBookAndChapter(book, chapter)
                 .orElseThrow(() -> new NoSuchElementException("존재하는 성경이 없습니다 : [" + "book : " + book + "]")), HttpStatus.OK);
     }
 
     @GetMapping("/kor/{short_label}/{chapter}")
-    public ResponseEntity<Object> getBibleBy(@PathVariable(name = "short_label") String short_label, @PathVariable(name = "chapter") Integer chapter) {
+    public ResponseEntity<Object> getKorBibleByShortLabelAndChapter(@PathVariable(name = "short_label") String short_label, @PathVariable(name = "chapter") Integer chapter) {
         if (short_label.matches(REGEX_KOR)) {
             return new ResponseEntity<>(bibleService.findKorByShortLabelAndChapter(short_label, chapter)
                     .orElseThrow(() -> new NoSuchElementException("존재하는 성경이 없습니다 : [" + "short_label : " + short_label + "]")), HttpStatus.OK);
