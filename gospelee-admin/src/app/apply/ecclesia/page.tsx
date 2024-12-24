0726baa2e86babdfca6948f049865cf3e0bd6f9c
jej8076 <jej8076@gmail.com>
2024-12-24 16:52:27 +0900

    교회등록신청 페이지 진행중2

--- gospelee-admin/src/app/apply/ecclesia/page.tsx
+++ gospelee-admin/src/app/apply/ecclesia/page.tsx
@@ -1,11 +1,19 @@
 'use client'
 
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {ChevronDownIcon} from '@heroicons/react/16/solid'
 import {Field, Label, Switch} from '@headlessui/react'
+import {AuthItems} from "~/constants/auth-items";
 
 export default function Example() {
-  const [agreed, setAgreed] = useState(false)
+  const [agreed, setAgreed] = useState(false);
+  const [pastor, setPastor] = useState("");
+
+  useEffect(() => {
+    const authInfo: string | null = localStorage.getItem(AuthItems.LastAuthInfo);
+    const authInfoObj = authInfo ? JSON.parse(authInfo) : null;
+    setPastor(authInfoObj?.name);
+  }, []);
 
   return (
       <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
@@ -28,6 +36,16 @@ export default function Example() {
         </div>
         <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
           <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            <div className="sm:col-span-2">
+              <label htmlFor="church-name" className="block text-sm/6 font-semibold text-gray-900">
+                담임목사
+              </label>
+              <div className="mt-2.5">
+                <div>
+                  {pastor}
+                </div>
+              </div>
+            </div>
             <div className="sm:col-span-2">
               <label htmlFor="church-name" className="block text-sm/6 font-semibold text-gray-900">
                 Church
@@ -71,35 +89,19 @@ export default function Example() {
               </div>
             </div>
             <div className="sm:col-span-2">
-              <label htmlFor="phone-number" className="block text-sm/6 font-semibold text-gray-900">
-                Phone number
+              <label htmlFor="identification-number"
+                     className="block text-sm/6 font-semibold text-gray-900">
+                교회고유번호
               </label>
               <div className="mt-2.5">
                 <div
                     className="flex rounded-md bg-white outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
-                  <div className="grid shrink-0 grid-cols-1 focus-within:relative">
-                    <select
-                        id="country"
-                        name="country"
-                        autoComplete="country"
-                        aria-label="Country"
-                        className="col-start-1 row-start-1 w-full appearance-none rounded-md py-2 pr-7 pl-3.5 text-base text-gray-500 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                    >
-                      <option>US</option>
-                      <option>CA</option>
-                      <option>EU</option>
-                    </select>
-                    <ChevronDownIcon
-                        aria-hidden="true"
-                        className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4"
-                    />
-                  </div>
                   <input
-                      id="phone-number"
-                      name="phone-number"
+                      id="identification-number"
+                      name="identification-number"
                       type="text"
-                      placeholder="123-456-7890"
-                      className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6 border border-gray-300"
+                      autoComplete="organization"
+                      className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 border border-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
                   />
                 </div>
               </div>
@@ -133,9 +135,9 @@ export default function Example() {
                 </Switch>
               </div>
               <Label className="text-sm/6 text-gray-600">
-                By selecting this, you agree to our{' '}
+                개인정보 보호 정책에{' '}
                 <a href="#" className="font-semibold text-indigo-600">
-                  privacy&nbsp;policy
+                  동의합니다
                 </a>
                 .
               </Label>
@@ -146,7 +148,7 @@ export default function Example() {
                 type="submit"
                 className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Lets talk
+              신청
             </button>
           </div>
         </form>
