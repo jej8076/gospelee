b29a5ba46e46aadbf09f66746c00a8431eefabbf
jej8076 <jej8076@gmail.com>
2025-02-08 12:37:22 +0900

    ecclesia page파일 push 누락

--- gospelee-admin/src/app/ecclesia/page.tsx
+++ gospelee-admin/src/app/ecclesia/page.tsx
@@ -7,10 +7,12 @@ import {AuthItems} from "~/constants/auth-items";
 import {apiFetch} from "~/lib/api-client";
 
 type Ecclesias = {
-  uid: bigint,
-  name: string,
-  masterAccountUid: string,
-  churchIdentificationNumber: string
+  ecclesiaUid: bigint,
+  churchIdentificationNumber: string,
+  status: string,
+  ecclesiaName: string,
+  masterAccountName: string,
+  insertTime: string,
   // name: string,
   // email: string,
   // role: string,
@@ -35,7 +37,6 @@ type Ecclesias = {
 
 
 export default function Ecclesia() {
-
   useAuth();
 
   const [ecc, setEcc] = useState<Ecclesias[]>([]);
@@ -62,48 +63,81 @@ export default function Ecclesia() {
   }, []);
 
   return (
-      <ul role="list" className="divide-y divide-gray-100">
-        {!(ecc.length > 0) ? null : ecc.map((e) => (
-            <li key={e.uid} className="relative flex justify-between gap-x-6 py-5">
-              <div className="flex min-w-0 gap-x-4">
-                <img className="h-12 w-12 flex-none rounded-full bg-gray-50"
-                     src={"https://i.ibb.co/zG2H4xG/2024-12-18-8-51-45.png"}
-                     alt=""/>
-                <div className="min-w-0 flex-auto">
-                  <p className="text-sm font-semibold leading-6 text-gray-900">
-                    <a href={"https://i.ibb.co/zG2H4xG/2024-12-18-8-51-45.png"}>
-                      <span className="absolute inset-x-0 -top-px bottom-0"/>
-                      {e.name}
-                    </a>
-                  </p>
-                  <p className="mt-1 flex text-xs leading-5 text-gray-500">
-                    <a href={`mailto:${e.masterAccountUid}`}
-                       className="relative truncate hover:underline">
-                      {e.name}
-                    </a>
-                  </p>
-                </div>
-              </div>
-              {/*<div className="flex shrink-0 items-center gap-x-4">*/}
-              {/*  <div className="hidden sm:flex sm:flex-col sm:items-end">*/}
-              {/*    <p className="text-sm leading-6 text-gray-900">{person.role}</p>*/}
-              {/*    {person.lastSeen ? (*/}
-              {/*        <p className="mt-1 text-xs leading-5 text-gray-500">*/}
-              {/*          Last seen <time dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>*/}
-              {/*        </p>*/}
-              {/*    ) : (*/}
-              {/*        <div className="mt-1 flex items-center gap-x-1.5">*/}
-              {/*          <div className="flex-none rounded-full bg-emerald-500/20 p-1">*/}
-              {/*            <div className="h-1.5 w-1.5 rounded-full bg-emerald-500"/>*/}
-              {/*          </div>*/}
-              {/*          <p className="text-xs leading-5 text-gray-500">Online</p>*/}
-              {/*        </div>*/}
-              {/*    )}*/}
-              {/*  </div>*/}
-              {/*  <ChevronRightIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true"/>*/}
-              {/*</div>*/}
-            </li>
-        ))}
-      </ul>
-  )
+      <div className="px-4 sm:px-6 lg:px-8">
+        <div className="sm:flex sm:items-center">
+          <div className="sm:flex-auto">
+            <h1 className="text-base font-semibold leading-6 text-gray-900">Users</h1>
+            <p className="mt-2 text-sm text-gray-700">
+              A list of all the users in your account including their name, title, email and role.
+            </p>
+          </div>
+          <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+            <button
+                type="button"
+                className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Add user
+            </button>
+          </div>
+        </div>
+        <div className="mt-8 flow-root">
+          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+              <table className="min-w-full divide-y divide-gray-300">
+                <thead>
+                <tr>
+                  <th scope="col"
+                      className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                    교회
+                  </th>
+                  <th scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    관리자
+                  </th>
+                  <th scope="col"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    상태
+                  </th>
+                  <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                    <span className="sr-only">Edit</span>
+                  </th>
+                </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 bg-white">
+                {!(ecc.length > 0) ? null : ecc.map((u) => (
+                    <tr key={u.ecclesiaUid}>
+                      <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
+                        <div className="flex items-center">
+                          <div className="h-11 w-11 flex-shrink-0">
+                            <img className="h-11 w-11 rounded-full" src="" alt=""/>
+                          </div>
+                          <div className="ml-4">
+                            <div className="font-medium text-gray-900">{u.ecclesiaName}</div>
+                            <div className="mt-1 text-gray-500">{u.churchIdentificationNumber}</div>
+                          </div>
+                        </div>
+                      </td>
+                      {/*<td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">*/}
+                      {/*  <div className="text-gray-900">{u.churchIdentificationNumber}</div>*/}
+                      {/*  <div className="mt-1 text-gray-500">{u.name}</div>*/}
+                      {/*</td>*/}
+                      <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">{u.masterAccountName}</td>
+                      <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                      <span
+                          className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                        Active
+                      </span>
+                      </td>
+                      <td className="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                        <a href="#" className="text-indigo-600 hover:text-indigo-900">Edit</a>
+                      </td>
+                    </tr>
+                ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+  );
 }
