443ec710073c034e5b87de742dd918294ae039e2
jej8076 <jej8076@gmail.com>
2025-04-18 17:05:43 +0900

    ecclesia 관리 페이지 문구 수정

--- gospelee-admin/src/app/ecclesia/page.tsx
+++ gospelee-admin/src/app/ecclesia/page.tsx
@@ -103,7 +103,7 @@ export default function Ecclesia() {
             {/*    className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"*/}
             {/*>*/}
             {/*  Add user*/}
-            {/*</button>*/}w
+            {/*</button>*/}
           </div>
         </div>
         <div className="mt-8 flow-root">
