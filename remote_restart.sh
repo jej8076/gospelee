5c79017c686d9d22b1529740d8594df7e6c92998
jej8076 <jej8076@gmail.com>
2025-04-25 17:31:12 +0900

    배포 스크립트 수정

--- remote_restart.sh
+++ remote_restart.sh
@@ -7,12 +7,9 @@ OOG_DEPLOY=/home/ubuntu/api
 PEM_FILE=/Users/jej/dev/aws/oog.pem
 
 # jar 파일 파일명
-JAR_FILE=gospelee-api.jar
+JAR_FILE=gospelee-api-0.0.1-SNAPSHOT.jar
 
 # aws 접속 정보
 USER_HOST=ubuntu@13.124.200.73
 
-./gradlew gospelee-api:bootJar
-
-scp -i $PEM_FILE ./gospelee-api/build/libs/$JAR_FILE $USER_HOST:$OOG_DEPLOY
 ssh -i $PEM_FILE $USER_HOST ${OOG_DEPLOY}/restart.sh
