1c88aae06af2a44bccca0726c7b1946bf68182b7
jej8076 <jej8076@gmail.com>
2025-07-18 23:48:40 +0900

    admin docker image ghrc 빌드

--- gospelee-admin/next.config.mjs
+++ gospelee-admin/next.config.mjs
@@ -1,13 +1,14 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   async rewrites() {
-//     return [
-//       {
-//         source: '/api/:path*',
-//         destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`,
-//       },
-//     ];
-//   },
-// };
-//
-// export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
