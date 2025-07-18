4199d4d923fe238f2d571a4c91be536d46e38214
jej8076 <jej8076@gmail.com>
2025-07-19 00:06:27 +0900

    next.config.mjs 에러 수정

--- gospelee-api/src/main/java/com/gospelee/api/config/SecurityConfig.java
+++ gospelee-api/src/main/java/com/gospelee/api/config/SecurityConfig.java
@@ -2,8 +2,6 @@ package com.gospelee.api.config;
 
 import com.gospelee.api.auth.jwt.JwtAuthenticationFilter;
 import com.gospelee.api.properties.CorsProperties;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
