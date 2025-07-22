16aa8c78c3c084fe9aa067833013ad2fe4d6e390
jej8076 <jej8076@gmail.com>
2025-07-22 17:53:41 +0900

    npm run build 시 에러 및 경고 수정

--- gospelee-api/src/main/java/com/gospelee/api/repository/jpa/account/AccountEcclesiaHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gospelee.api.repository.jpa.account;
-
-import com.gospelee.api.entity.AccountEcclesiaHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountEcclesiaHistoryRepository extends
-    JpaRepository<AccountEcclesiaHistory, Long> {
-
-}
