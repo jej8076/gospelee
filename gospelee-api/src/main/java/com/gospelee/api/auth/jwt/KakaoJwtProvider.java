bed552cf911170fc8e4bd49f19aa81427a146ba5
jej8076 <jej8076@gmail.com>
2026-01-03 21:46:22 +0900

    클로드 md 추가, 로그인 토큰 변경 및 헤더 대소문자 실수 수정

--- gospelee-api/src/main/java/com/gospelee/api/auth/jwt/KakaoJwtProvider.java
+++ gospelee-api/src/main/java/com/gospelee/api/auth/jwt/KakaoJwtProvider.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gospelee.api.dto.jwt.JwkDTO;
 import com.gospelee.api.dto.jwt.JwkSetDTO;
 import com.gospelee.api.dto.jwt.JwtPayload;
-import com.gospelee.api.enums.RedisCacheNames;
 import com.gospelee.api.enums.SocialLoginPlatform;
 import com.gospelee.api.service.AccountService;
 import com.gospelee.api.service.RedisCacheService;
@@ -132,15 +131,15 @@ public class KakaoJwtProvider extends SocialJwtProvider {
     }
 
     // TODO 앱스토어 심사용 임시로 비활성화
-    if (nonceCacheKey != null) {
-      String cachedNonce = redisCacheService.get(RedisCacheNames.NONCE, nonceCacheKey);
-      String nonce = String.valueOf(map.get("nonce"));
-      if (!nonce.equals(cachedNonce)) {
-        log.error("일치하는 nonce값이 없습니다. [platform: {}, nonceCacheKey: {}, nonce: {}]", "kakao",
-            nonceCacheKey, nonce);
-        return false;
-      }
-    }
+//    if (nonceCacheKey != null) {
+//      String cachedNonce = redisCacheService.get(RedisCacheNames.NONCE, nonceCacheKey);
+//      String nonce = String.valueOf(map.get("nonce"));
+//      if (!nonce.equals(cachedNonce)) {
+//        log.error("일치하는 nonce값이 없습니다. [platform: {}, nonceCacheKey: {}, nonce: {}]", "kakao",
+//            nonceCacheKey, nonce);
+//        return false;
+//      }
+//    }
 
     return true;
   }
